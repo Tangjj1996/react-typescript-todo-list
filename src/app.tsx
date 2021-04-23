e7e927047b11a0bc85d3ff96d51b4d54a929a73c
Tangjj1996 <tangjj_1996@163.com>
2021-04-23 15:00:40 +0800

    feat: add router

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,31 @@
 import * as React from 'react'
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './views/home'
+import About from './views/about'
 interface appProp {
   name: string
 }
 const App: React.FC<appProp> = ({ name }) => {
-  return <div>this is a test {name}.</div>
+  return (
+    <Router>
+      <Link to="/home">go to home</Link>
+      <Link to="/aboutss">go to about</Link>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <div>404</div>
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/views/home.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const Home = () => {
+  return <div>this is home</div>
+}
+
+export default Home
